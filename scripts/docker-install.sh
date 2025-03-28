4e2c3184f9b0401d46bdc8fea4beb8420bb8a9d4
kimjisub <0226daniel@gmail.com>
2025-03-28 15:15:24 +0900

    ✨ New docker installation

--- scripts/docker-install.sh
+++ scripts/docker-install.sh
@@ -1,38 +1,26 @@
 #!/bin/bash
 
-# Docker 설치를 위한 패키지 업데이트
+# https://docs.docker.com/engine/install/ubuntu/#installation-methods
+
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 
-# HTTPS를 통해 저장소를 사용할 수 있도록 패키지 설치
-sudo apt-get install -y \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    software-properties-common
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-# Docker의 공식 GPG 키 추가
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-
-# Docker 저장소를 APT 소스에 추가
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
-
-# Docker 패키지 업데이트
-sudo apt-get update
-
-# Docker CE(Community Edition) 설치
-sudo apt-get install -y docker-ce
-
-# Docker가 정상적으로 설치되었는지 버전 확인
 docker --version
 
-# Docker 그룹에 사용자 추가
 sudo usermod -aG docker $USER
-
 
 touch ~/.docker/config.json
 echo "{}" > ~/.docker/config.json
-
-# ssh 재접속
